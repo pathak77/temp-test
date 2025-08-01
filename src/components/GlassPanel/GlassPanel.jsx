8f9d2758361ddaabbea2082bb4f293f085b4d882
Aayush Pathak <aayushpathak77@gmail.com>
2025-08-02 02:12:35 +0545

    added items tab

---
+++ src/components/GlassPanel/GlassPanel.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const GlassPanel = () => {
+  return (
+    <div
+      className="absolute ml-[30%] mr-[30%] w-[476px] h-[500px] bg-white bg-opacity-20 backdrop-blur-md rounded-lg border border-white border-opacity-30 shadow-lg"
+      style={{
+        WebkitBackdropFilter: "blur(10px)",
+        backdropFilter: "blur(10px)",
+      }}
+    >
+    <p>
+     </p>
+    </div>
+  );
+};
+
+export default GlassPanel;
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState, React} from "react";
 import Complementry from "../../assets/img/complementry.jpeg";
 import Exchange from "../../assets/img/easy_exchange.jpeg";
 import Wrapping from "../../assets/img/gift_wrapping.jpeg";
@@ -6,24 +6,50 @@ import GlassPanel from "../GlassPanel/GlassPanel";
 import { Expand } from "lucide-react";
 
 const Items = () => {
+  const panelItem = [
+    {id:1,
+      text:"fas",
+      header:'head'
+    }
+  ]
+
+
+  const [click, setClick] = useState(false);
+
+  const setAction = ()=>{
+
+  }
+
   return (
       <div className=" relative bg-[#F9F9F9] min-h-[12rem] min-w-full m-0 border-2 border-black grid grid-cols-3 gap-4">
        
+       <button onClick={setAction}>
         <img
           src={Complementry}
           className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
           alt="complementry_item"
         />
-        <img
+       </button>
+        
+        <button onClick={setAction}>
+          <img
           src={Wrapping}
           className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
           alt="Wrapping"
         />
-        <img
+        </button>
+        
+        <button onClick={setAction}>
+          <img
           src={Exchange}
           className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
           alt="exchange"
-        /> <GlassPanel />
+        /> 
+        </button>
+        
+        {click &&
+        <GlassPanel />
+        }
       </div>
       
   
