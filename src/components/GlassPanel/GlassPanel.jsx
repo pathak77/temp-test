054872d4b0785ce07201922dd381fb6d968d8b11
Aayush Pathak <aayushpathak77@gmail.com>
2025-08-02 02:54:25 +0545

    fixed all paneling on foryou hero section

--- src/components/GlassPanel/GlassPanel.jsx
+++ src/components/GlassPanel/GlassPanel.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import DeleteIcon from "../../assets/icons/DeleteIcon";
 
-const GlassPanel = () => {
+const GlassPanel = ({ header = "", text = "", visible = false, onClose  }) => {
   return (
     <div
-      className="absolute ml-[30%] mr-[30%] w-[476px] h-[500px] bg-white bg-opacity-20 backdrop-blur-md rounded-lg border border-white border-opacity-30 shadow-lg"
+      className={` ${
+        visible ? "absolute" : "hidden"
+      } left-1/2 transform -translate-x-1/2 w-[476px] h-[500px] bg-white bg-opacity-20 backdrop-blur-md rounded-lg border border-white border-opacity-30 shadow-lg`}
       style={{
         WebkitBackdropFilter: "blur(10px)",
         backdropFilter: "blur(10px)",
       }}
     >
-    <p>
-     </p>
+      <button 
+        onClick={onClose}
+        className="absolute top-5 right-5  cursor-pointer" >
+        <DeleteIcon />
+      </button>
+      <h1>{header}</h1>
+      <p>{text}</p>
     </div>
   );
 };
--- src/components/Items/Items.jsx
+++ src/components/Items/Items.jsx
@@ -1,58 +1,61 @@
-import {useState, React} from "react";
+import { useState, React } from "react";
 import Complementry from "../../assets/img/complementry.jpeg";
 import Exchange from "../../assets/img/easy_exchange.jpeg";
 import Wrapping from "../../assets/img/gift_wrapping.jpeg";
 import GlassPanel from "../GlassPanel/GlassPanel";
-import { Expand } from "lucide-react";
+
 
 const Items = () => {
   const panelItem = [
-    {id:1,
-      text:"fas",
-      header:'head'
-    }
-  ]
-
+    { id: 1, text: "aaa", header: "head1" },
+    { id: 2, text: "bbb", header: "head2" },
+    { id: 3, text: "ccc", header: "head3" },
+  ];
 
   const [click, setClick] = useState(false);
+  const [itemIndex, setItemIndex] = useState(0);
 
-  const setAction = ()=>{
-
-  }
+  const setAction = (index) => {
+    setClick(!click);
+    setItemIndex(index);
+  };
 
   return (
-      <div className=" relative bg-[#F9F9F9] min-h-[12rem] min-w-full m-0 border-2 border-black grid grid-cols-3 gap-4">
-       
-       <button onClick={setAction}>
+    <div className=" relative bg-[#F9F9F9] min-h-[12rem] min-w-full m-0  grid grid-cols-3 gap-4 rounded-lg">
+      <button onClick={() => setAction(0)}>
         <img
           src={Complementry}
-          className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
+          className="overflow-hidden  hover:-translate-y-2 transition-transform ease-in-out duration-300 hover:cursor-pointer"
           alt="complementry_item"
         />
-       </button>
-        
-        <button onClick={setAction}>
-          <img
+      </button>
+
+      <button onClick={() => setAction(1)}>
+        <img
           src={Wrapping}
-          className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
+          className="overflow-hidden  hover:-translate-y-2 transition-transform ease-in-out duration-300 hover:cursor-pointer"
           alt="Wrapping"
         />
-        </button>
-        
-        <button onClick={setAction}>
-          <img
+      </button>
+
+      <button onClick={() => setAction(2)}>
+        <img
           src={Exchange}
-          className="overflow-hidden  hover:-translate-y-2 transition-tranform ease-in-out duration-300 hover:cursor-pointer"
+          className="overflow-hidden  hover:-translate-y-2 transition-transform ease-in-out duration-300 hover:cursor-pointer"
           alt="exchange"
-        /> 
-        </button>
-        
-        {click &&
-        <GlassPanel />
-        }
-      </div>
-      
-  
+        />
+      </button>
+
+     
+          
+          <GlassPanel
+            header={panelItem[itemIndex].header}
+            text={panelItem[itemIndex].text}
+            visible = {click}
+            onClose={()=> setClick(false)}
+          />
+       
+    </div>
   );
 };
 
