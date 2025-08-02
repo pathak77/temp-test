165df62c920be835db8392a25c24c9163481b6a4
Aayush Pathak <aayushpathak77@gmail.com>
2025-08-03 00:20:55 +0545

    made adjustments

---
+++ src/assets/icons/DeleteIcon.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DeleteIcon = () => {
+  return (
+    <svg
+      className="hover:-translate-y-1 transition-transform duration-150 ease-linear"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path d="M10.4951 11.5557L5.02539 17.0254L6.08605 18.086L11.5557 12.6164L17.0254 18.0861L18.086 17.0254L12.6164 11.5557L18.086 6.08605L17.0254 5.02539L11.5557 10.4951L6.08605 5.0254L5.02539 6.08606L10.4951 11.5557Z"></path>
+    </svg>
+  );
+};
+
+export default DeleteIcon;
---
+++ src/assets/icons/SearchIcon.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SearchIcon = () => {
+  return (
+    <div>
+      <svg
+        class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium DS-Icon mui-latin-u81msc"
+        focusable="false"
+        color="black"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+      >
+        <path
+          fill="currentColor"
+         
+          d="M5.7 11a5.3 5.3 0 1 1 10.6 0 5.3 5.3 0 0 1-10.6 0M11 4.3a6.7 6.7 0 1 0 4.217 11.907l2.288 2.288a.7.7 0 0 0 .99-.99l-2.288-2.288A6.7 6.7 0 0 0 11 4.3"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default SearchIcon;
